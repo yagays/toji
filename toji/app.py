ca44d795b8b60cecf42cd16bff8736ef21f9834e
Yuki Okuda <y.okuda@dr-ubie.com>
2022-03-03 21:45:56 +0900

    update texts

--- toji/app.py
+++ toji/app.py
@@ -33,10 +33,10 @@ def main():
 
     col1, col2 = st.columns(2)
     with col1:
-        if st.button("previous"):
+        if st.button("< Previous"):
             st.session_state["counter"].previous()
     with col2:
-        if st.button("next"):
+        if st.button("Next >"):
             st.session_state["counter"].next()
 
     if all_text:
